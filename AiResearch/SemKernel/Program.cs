5725c7a6b02c1fde00e10c3bd3ff63fe031961a7
Sean Grimes <sean.grimes@swimlane.com>
2025-06-11 12:45:52 -0500

    Working function calling with Mistral Connector

--- AiResearch/SemKernel/Program.cs
+++ AiResearch/SemKernel/Program.cs
@@ -2,12 +2,42 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.MistralAI;
 using Microsoft.SemanticKernel.Connectors.MistralAI.Client;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 const string modelId = "mistralai/Mistral-Small-3.1-24B-Instruct-2503";
 var endpoint = new Uri("http://54.184.10.215:3000/v1/");
 const string apiKey = "-";
+
+var resourceBuilder = ResourceBuilder
+    .CreateDefault()
+    .AddService("SemKernel");
+
+using var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddSource("Microsoft.SemanticKernel*")
+    .AddConsoleExporter()
+    .Build();
+
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    // Add OpenTelemetry as a logging provider
+    builder.AddOpenTelemetry(options =>
+    {
+        options.SetResourceBuilder(resourceBuilder);
+        options.AddConsoleExporter();
+        // Format log messages. This is default to false.
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+    });
+    builder.SetMinimumLevel(LogLevel.Trace);
+});
      
 
 #pragma warning disable SKEXP0070
@@ -22,7 +52,7 @@ builder.Plugins.AddFromFunctions("component_execution", [
             return "42 degrees";
         },
         functionName: "get_weather",
-        description: "The city and state, e.g. Boston, MA",
+        description: "Gets the weather for a location, Boston, MA for example",
         parameters: [
             new KernelParameterMetadata("city") { Description = "The city to get the weather for" },
             new KernelParameterMetadata("state") { Description = "The state to get the weather for" },
@@ -30,14 +60,17 @@ builder.Plugins.AddFromFunctions("component_execution", [
     )
 ]);
 
+builder.Services.AddSingleton(loggerFactory);
+
 var kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 var history = new ChatHistory();
 history.AddSystemMessage("You are a helpful assistant");
 
-var executionSettings = new OpenAIPromptExecutionSettings
+var executionSettings = new MistralAIPromptExecutionSettings
 {
-    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(autoInvoke: false)
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
+    ToolCallBehavior = MistralAIToolCallBehavior.AutoInvokeKernelFunctions
 };
 
 // Initiate a back-and-forth chat
@@ -50,16 +83,12 @@ do {
     // Add user input
     history.AddUserMessage(userInput!);
     
-    
-
     // Get the response from the AI
     var result = await chatCompletionService.GetChatMessageContentAsync(
         history,
         executionSettings: executionSettings,
         kernel: kernel);
     
-    
-
     // Print the results
     Console.WriteLine("Assistant > " + result);
     
